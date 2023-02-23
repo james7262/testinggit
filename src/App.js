9b329422a3c43415c5a75a7b3945ebe24dec755c
James Bachrach <bachrachj@chc.edu>
2023-02-23 18:54:46 -0500

    Version 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from "react";
+import Countdown from "react-countdown";
+
+const Completionist = () => <span>You are good to go!</span>;
+
+const renderer = ({hours, minutes, seconds, completed}) => {
+  if (completed) {
+    return <Completionist />;
+  } else {
+    return <span>{hours}:{minutes}:{seconds}</span>
+  }
+};
 
 function App() {
   return (
     <div>
-      <h2>Countdown</h2>
+      <h2 style = {{textAlign : 'center'}}>Countdown</h2>
+      <div style = {{textAlign : 'center'}}>
+        <Countdown
+          date = {Date.now() + 10000}
+          renderer = {renderer}
+        >
+          <Completionist />
+        </Countdown>
+      </div>
     </div>
   );
 }
